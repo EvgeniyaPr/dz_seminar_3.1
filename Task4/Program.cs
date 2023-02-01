5b233fa1ea5cd319d4195534ad097900cd01113b
Evgeniya <prahova.e@ya.ru>
2023-02-01 21:51:27 +0500

    task4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -12,4 +12,11 @@ int numberB = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число c: ");
 int numberC = Convert.ToInt32(Console.ReadLine());
 
+int max = numberA;
 
+if (max < numberB) max = numberB;
+
+if (max < numberC) max = numberC;
+{
+  Console.Write("max = " + max);
+}
